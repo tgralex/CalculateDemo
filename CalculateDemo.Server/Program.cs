0d3f7114216fb8ef7c137b644fa42a5f16d347db
Tigran Aleksanyan <tgralex@hotmail.com>
2024-05-04 10:07:20 -0400

    adding NLogging (prelim)

--- CalculateDemo.Server/Program.cs
+++ CalculateDemo.Server/Program.cs
@@ -1,86 +1,129 @@
 using AspNetCoreRateLimit;
-//using Microsoft.Extensions.Logging;
-//using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.AspNetCore.HttpLogging;
+using NLog;
+using NLog.Web;
 
-
-var builder = WebApplication.CreateBuilder(args);
-// Configure logging to use a file-based logger
-
-//builder.Logging.AddFile(options =>
-//{
-//    options.FileName = "app-logs.txt";
-//    options.FileSizeLimit = 1024 * 1024 * 5; // 5 MB
-//    options.RetainedFileCountLimit = 7;
-//});
-
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var services = builder.Services;
 const string allowCorsOnDevelopmentName = nameof(allowCorsOnDevelopmentName);
-services.AddCors(options =>
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("Application has started");
+try
 {
-    // allowing CORS policy from client development port
-    options.AddPolicy(allowCorsOnDevelopmentName,
-        policyBuilder =>
-        {
-            policyBuilder
-                .WithOrigins("https://localhost:4200")
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials();
-        });
-});
-services.AddMemoryCache();
-services.Configure<IpRateLimitOptions>(options =>
+    BuildServicesAndApp();
+}
+catch (Exception exception)
 {
-    options.EnableEndpointRateLimiting = true;
-    options.StackBlockedRequests = false;
-    options.HttpStatusCode = 429;
-    options.RealIpHeader = "X-Real-IP";
-    options.ClientIdHeader = "X-ClientId";
-    options.GeneralRules = new List<RateLimitRule>
-    {
-        // one request per second
-        new()
-        {
-            Endpoint = "PUT:/api/Calculate",
-            Period = "1s",
-            Limit = 1,
-        }
-    };
-});
-services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
-services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
-services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
-services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
-services.AddInMemoryRateLimiting();
-
-/**************************************************************/
-
-var app = builder.Build();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    logger.Debug("Application has ended");
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    LogManager.Shutdown();
+}
+void BuildServicesAndApp()
+{
+    var builder = WebApplication.CreateBuilder(args);
+   
+    BuildServices(builder);
+
+    var app = builder.Build();
+    BuildApp(app);
+
+    app.Run();
 }
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.MapFallbackToFile("/index.html");
+void BuildServices(IHostApplicationBuilder builder)
+{
+    var services = builder.Services;
 
-app.UseCors(allowCorsOnDevelopmentName);
-app.UseIpRateLimiting();
+    // Add services to the container.
+    services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
 
-app.Run();
+    AddCorsServiceForDevelopment(services);
+    AddIpRateLimitationService(services);
+
+    services.AddHttpLogging(logging => { logger.Info("logging");});
+    //{
+    //    logging.LoggingFields = HttpLoggingFields.All;
+    //    logging.RequestHeaders.Add("sec-ch-ua");
+    //    logging.ResponseHeaders.Add("MyResponseHeader");
+    //    logging.MediaTypeOptions.AddText("application/javascript");
+    //    logging.RequestBodyLogLimit = 4096;
+    //    logging.ResponseBodyLogLimit = 4096;
+    //    logging.CombineLogs = true;
+    //});
+}
+
+void AddCorsServiceForDevelopment(IServiceCollection services)
+{
+    services.AddCors(options =>
+    {
+        // allowing CORS policy from client development port
+        options.AddPolicy(allowCorsOnDevelopmentName,
+            policyBuilder =>
+            {
+                policyBuilder
+                    .WithOrigins("https://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+    });
+}
+
+void AddIpRateLimitationService(IServiceCollection services)
+{
+    // Adding IP Rate Limitation (requires MemoryCache)
+    services.AddMemoryCache();
+    services.Configure<IpRateLimitOptions>(options =>
+    {
+        options.EnableEndpointRateLimiting = true;
+        options.StackBlockedRequests = false;
+        options.HttpStatusCode = 429;
+        options.RealIpHeader = "X-Real-IP";
+        options.ClientIdHeader = "X-ClientId";
+        options.GeneralRules = new List<RateLimitRule>
+        {
+            // one request per second
+            new()
+            {
+                Endpoint = "PUT:/api/Calculate",
+                Period = "1s",
+                Limit = 1,
+            }
+        };
+    });
+    services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+    services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+    services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+    services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+    services.AddInMemoryRateLimiting();
+}
+
+void BuildApp(WebApplication app)
+{
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+    app.MapControllers();
+    app.MapFallbackToFile("/index.html");
+
+    // Ensures that HTTPS is the only protocol allowed
+    app.UseHttpsRedirection();
+    app.UseCors(allowCorsOnDevelopmentName);
+    app.UseIpRateLimiting();
+    app.UseHttpLogging();
+}
